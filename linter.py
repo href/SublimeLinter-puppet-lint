202f0bd45d47059cc5d55a4b703afd059194aead
Denis Krienbühl <denis@href.ch>
2018-01-09 10:54:42 +0100

    Replace linter with something that works

--- linter.py
+++ linter.py
@@ -2,8 +2,8 @@
 # linter.py
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
-# Written by Andrew Grim
-# Copyright (c) 2014 Andrew Grim
+# Written by Denis Krienbühl
+# Copyright (c) 2018 Denis Krienbühl
 #
 # License: MIT
 #
@@ -14,17 +14,26 @@ from SublimeLinter.lint import Linter, util
 
 
 class PuppetLint(Linter):
-
     """Provides an interface to puppet-lint."""
 
     syntax = 'puppet'
     cmd = ('puppet-lint', '--log-format', '%{line}:%{column}:%{kind}:%{message}', '*')
     executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
     regex = (
         r'^(?P<line>\d+):(?P<col>\d+):'
         r'((?P<warning>warning)|(?P<error>error)):'
         r'(?P<message>.+)'
     )
+    multiline = False
+    line_col_base = (1, 1)
     tempfile_suffix = '-'
-    error_stream = util.STREAM_STDOUT
-    word_re = r'^(".*?"|[-\w]+)'
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = None
