db9c412c4afe5eed6307a88ef81b2ca76e39d869
Denis Krienbühl <denis@href.ch>
2019-08-14 15:44:15 +0200

    Removes deprecated settings

--- linter.py
+++ linter.py
@@ -20,10 +20,7 @@ class PuppetLint(Linter):
         'selector': 'source.puppet'
     }
     cmd = ('puppet-lint', '--log-format', '%{line}:%{column}:%{kind}:%{message}', '*')
-    executable = None
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 1.0'
+
     regex = (
         r'^(?P<line>\d+):(?P<col>\d+):'
         r'((?P<warning>warning)|(?P<error>error)):'
@@ -33,9 +30,4 @@ class PuppetLint(Linter):
     line_col_base = (1, 1)
     tempfile_suffix = '-'
     error_stream = util.STREAM_BOTH
-    selectors = {}
     word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
